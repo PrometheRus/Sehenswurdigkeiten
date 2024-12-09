5263c4c0f84bdf112ed3b4e90b572c8c8275a6f7
valkovskii-n@work <valkovskii.n@selectel.ru>
2024-12-09 10:49:22 +0300

    Update code

--- Galera/1.Terraform/_vars.tf
+++ Galera/1.Terraform/_vars.tf
@@ -12,19 +12,19 @@ variable "selectel-project-id" {
 
 
 ### The service account is already created. Using its data
-variable "service-account-main-name" {
+variable "service-account-name" {
   default     = "{{ REPLACE ME }}"
   type        = string
   description = "The name of the service account"
 }
 
-variable "service-account-main-password" {
+variable "service-account-password" {
   default     = "{{ REPLACE ME }}"
   type        = string
   description = "The password of the service account"
 }
 
-variable "service-account-main-id" {
+variable "service-account-id" {
   default     = "{{ REPLACE ME }}"
   type        = string
   description = "The ID of the service account"
@@ -40,4 +40,22 @@ variable "image_id" {
   default     = "b671a80e-9bf0-4861-9833-bd711bd8a02f"    # Ubuntu 24.04
   type        = string
   description = "The ID of the image to use"
+}
+
+variable "availability_zone" {
+  default     = "ru-9a"
+  type        = string
+  description = "The availability zone to use"
+}
+
+variable "volume_type" {
+  default     = "basic.ru-9a"
+  type        = string
+  description = "The volume type to use"
+}
+
+variable "volume_size" {
+  default     = "15"
+  type        = string
+  description = "The volume size to use"
 }
