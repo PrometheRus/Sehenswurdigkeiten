ae27466fd5ae7f8fa1b6c7d48644c638169331ec
valkovskii-n@work <valkovskii.n@selectel.ru>
2024-12-13 12:26:19 +0300

    Update code

--- Devstack/Terraform/compute.tf
+++ Devstack/Terraform/compute.tf
@@ -29,53 +29,53 @@ resource "openstack_compute_instance_v2" "devstack_server_1" {
   }
 }
 
-# resource "openstack_compute_instance_v2" "devstack_server_2" {
-#   name              = "devstack_2"
-#   flavor_id         = var.flavor_id
-#   key_pair          = selectel_vpc_keypair_v2.keypair_1.name
-#   availability_zone = var.availability_zone
-#   user_data = file("./metadata/init.yml")
-#
-#   network {
-#     port = openstack_networking_port_v2.port_1_devstack_server_2.id
-#   }
-#
-#   network {
-#     port = openstack_networking_port_v2.port_2_devstack_server_2.id
-#   }
-#
-#   block_device {
-#     uuid                  = var.image_id
-#     volume_size           = var.volume_size
-#     source_type           = "image"
-#     boot_index            = 0
-#     destination_type      = "volume"
-#     delete_on_termination = true
-#   }
-# }
-#
-# resource "openstack_compute_instance_v2" "devstack_server_3" {
-#   name              = "devstack_3"
-#   flavor_id         = var.flavor_id
-#   key_pair          = selectel_vpc_keypair_v2.keypair_1.name
-#   availability_zone = var.availability_zone
-#   user_data = file("./metadata/init.yml")
-#
-#   network {
-#     port = openstack_networking_port_v2.port_1_devstack_server_3.id
-#   }
-#
-#   network {
-#     port = openstack_networking_port_v2.port_2_devstack_server_3.id
-#   }
-#
-#   block_device {
-#     uuid                  = var.image_id
-#     volume_size           = var.volume_size
-#     source_type           = "image"
-#     boot_index            = 0
-#     destination_type      = "volume"
-#     delete_on_termination = true
-#   }
-# }
+resource "openstack_compute_instance_v2" "devstack_server_2" {
+  name              = "devstack_2"
+  flavor_id         = var.flavor_id
+  key_pair          = selectel_vpc_keypair_v2.keypair_1.name
+  availability_zone = var.availability_zone
+  user_data = file("./metadata/init.yml")
+
+  network {
+    port = openstack_networking_port_v2.port_1_devstack_server_2.id
+  }
+
+  network {
+    port = openstack_networking_port_v2.port_2_devstack_server_2.id
+  }
+
+  block_device {
+    uuid                  = var.image_id
+    volume_size           = var.volume_size
+    source_type           = "image"
+    boot_index            = 0
+    destination_type      = "volume"
+    delete_on_termination = true
+  }
+}
+
+resource "openstack_compute_instance_v2" "devstack_server_3" {
+  name              = "devstack_3"
+  flavor_id         = var.flavor_id
+  key_pair          = selectel_vpc_keypair_v2.keypair_1.name
+  availability_zone = var.availability_zone
+  user_data = file("./metadata/init.yml")
+
+  network {
+    port = openstack_networking_port_v2.port_1_devstack_server_3.id
+  }
+
+  network {
+    port = openstack_networking_port_v2.port_2_devstack_server_3.id
+  }
+
+  block_device {
+    uuid                  = var.image_id
+    volume_size           = var.volume_size
+    source_type           = "image"
+    boot_index            = 0
+    destination_type      = "volume"
+    delete_on_termination = true
+  }
+}
 
