c095644929fc092d008c70d819fef8aadba70670
prometherus@butlerova <walkowski134@gmail.com>
2025-02-01 14:17:01 +0300

    Update code

--- Ceph/Terraform/_vars.tf
+++ Ceph/Terraform/_vars.tf
@@ -35,3 +35,45 @@ variable "ceph_volume_size" {
   type        = string
   description = "The volume size to use"
 }
+
+variable "domain" {
+  default     = "<domain-id>"
+  type        = string
+  description = "The id of the user account"
+  sensitive   = true
+}
+
+variable "project-id" {
+  default     = "<project-id>"
+  type        = string
+  description = "The id of the project"
+  sensitive   = true
+}
+
+variable "service-account-name" {
+  default     = "<name>"
+  type        = string
+  description = "The name of the service account"
+  sensitive   = true
+}
+
+variable "service-account-password" {
+  default     = "<password>"
+  type        = string
+  description = "The password of the service account"
+  sensitive   = true
+}
+
+variable "service-account-id" {
+  default     = "<account-id>"
+  type        = string
+  description = "The ID of the service account"
+  sensitive   = true
+}
+
+variable "service-ssh-key-name" {
+  default     = "<name>"
+  description = "Type the command and get the name: 'openstack keypair list'"
+  type        = string
+  sensitive   = true
+}
