fb55cdbc1ab53923616da994bc6fe5e7eee601da
prometherus@butlerova <walkowski134@gmail.com>
2025-02-01 14:33:00 +0300

    Update code

--- Ceph/Terraform/main.tf
+++ Ceph/Terraform/main.tf
@@ -14,7 +14,7 @@ terraform {
     endpoints = {
       s3 = "https://s3.ru-1.storage.selcloud.ru"
     }
-    key    = "terraform/state/terraform.tfstate"
+    key    = "terraform/ceph/terraform.tfstate"
     region = "ru-1"
 
     skip_region_validation      = true
